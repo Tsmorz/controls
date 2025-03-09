56d363ab85a6fbfb747bae2b5b7f6ac8a7d6589a
Tsmorz <tonysmoragiewicz@gmail.com>
2025-03-09 12:26:46 -0700

    add state space model class to kalman filter

--- src/modules/filter.py
+++ src/modules/filter.py
@@ -5,6 +5,7 @@ from typing import Optional
 
 import numpy as np
 from definitions import NUM_STEPS
+from state_space import StateSpace
 
 
 class KalmanFilter:
@@ -12,9 +13,7 @@ class KalmanFilter:
 
     def __init__(
         self,
-        A: np.ndarray,
-        B: np.ndarray,
-        C: np.ndarray,
+        state_space: StateSpace,
         Q: np.ndarray,
         R: np.ndarray,
         initial_state: np.ndarray,
@@ -22,18 +21,16 @@ class KalmanFilter:
     ) -> None:
         """Initialize the Kalman Filter.
 
-        :param A: State transition matrix
-        :param B: Control input matrix
-        :param C: Observation matrix
+        :param state_space: linear state space model
         :param Q: Process noise covariance
         :param R: Measurement noise covariance
         :param initial_state: Initial state estimate
         :param initial_covariance: Initial error covariance
         :return: None
         """
-        self.A: np.ndarray = A
-        self.B: np.ndarray = B
-        self.H: np.ndarray = C
+        self.A: np.ndarray = state_space.A
+        self.B: np.ndarray = state_space.B
+        self.C: np.ndarray = state_space.C
         self.Q: np.ndarray = Q
         self.R: np.ndarray = R
         self.P: np.ndarray = initial_covariance
@@ -56,11 +53,11 @@ class KalmanFilter:
 
         :param z: Measurement
         """
-        S = self.H @ self.P @ self.H.T + self.R  # Innovation covariance
-        K = self.P @ self.H.T @ np.linalg.inv(S)  # Kalman gain
-        y = z - self.H @ self.x  # Measurement residual
+        S = self.C @ self.P @ self.C.T + self.R  # Innovation covariance
+        K = self.P @ self.C.T @ np.linalg.inv(S)  # Kalman gain
+        y = z - self.C @ self.x  # Measurement residual
         self.x = self.x + K @ y
-        self.P = (np.eye(self.P.shape[0]) - K @ self.H) @ self.P
+        self.P = (np.eye(self.P.shape[0]) - K @ self.C) @ self.P
 
         self.history.append(self.x.copy())  # Store the updated state for plotting
 
@@ -83,40 +80,30 @@ def main():
         help="Directory to process.",
     )
 
-    A = np.array(
-        [
-            [1.0, 1.0],
-            [0.0, 1.0],
-        ]
+    state_space = StateSpace(
+        A=np.array([[1.0, 1.0], [0.0, 1.0]]),
+        B=np.array([[0.5], [1.0]]),
+        C=np.array([[1, 0], [0, 1]]),
     )
-    B = np.array(
-        [
-            [0.5],
-            [1.0],
-        ]
-    )
-    C = np.array(
-        [
-            [1, 0],
-            [0, 1],
-        ]
-    )  # Observation matrix
+    # Process noise covariance
     Q = np.array(
         [
             [1e-4, 0],
             [0, 1e-4],
         ]
-    )  # Process noise covariance
-    R = np.array([[1e-2]])  # Measurement noise covariance
-    P = np.eye(2)  # Initial error covariance
-    x = np.array(
-        [
-            [0],
-            [0],
-        ]
-    )  # Initial state estimate
+    )
+    # Measurement noise covariance
+    R = np.array([[1e-2]])
 
-    kf = KalmanFilter(A, B, C, Q, R, initial_state=x, initial_covariance=P)
+    kf = KalmanFilter(
+        state_space,
+        Q,
+        R,
+        initial_state=np.array([[0], [0]]),
+        initial_covariance=np.eye(2),
+    )
+
+    # Generate random control inputs and measurements and update the Kalman filter
     for _ in range(NUM_STEPS):
         m = np.random.rand(2, 1)
         kf.predict(u=np.array([[1]]))
