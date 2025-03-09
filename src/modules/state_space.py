15f958ebf2807c4b748ec466d10d75061f9e7b96
Tsmorz <tonysmoragiewicz@gmail.com>
2025-03-09 12:23:16 -0700

    add method to descritize state space model

--- src/modules/state_space.py
+++ src/modules/state_space.py
@@ -4,6 +4,8 @@ from typing import Optional
 
 import numpy as np
 
+DEFAULT_DT = 1.0
+
 
 class StateSpace:
     """A discrete-time state-space model representation."""
@@ -32,3 +34,27 @@ class StateSpace:
         if D is None:
             D = np.zeros((C.shape[0], B.shape[1]))
         self.D = D
+
+        self.dt = DEFAULT_DT  # Default time step
+
+    def continuous_to_discrete(self, delta_time: float = DEFAULT_DT) -> None:
+        self.dt = delta_time
+
+        A = matrix_exp(self.A, self.dt)
+        B = np.linalg.inv(self.A) @ (A - np.eye(2)) @ self.B
+
+        self.A = A
+        self.B = B
+
+
+def matrix_exp(matrix: np.ndarray, dt: float) -> np.ndarray:
+    """Find the matrix exponential.
+
+    :param matrix: Input matrix
+    :param dt: Time step
+    :return: Matrix exponential
+    """
+    d, U = np.linalg.eig(matrix)
+    D = np.exp(d * dt) * np.eye(np.shape(matrix)[0])
+    A = np.real(U @ D @ np.linalg.inv(U))
+    return A
